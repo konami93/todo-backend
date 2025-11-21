b5a7c52a6ccd1d832662465f7697ea6afbe02ef8
Yaniel <yanielsanchez930316@gmail.com>
2025-11-21 17:53:46 -0300

    import tasks module into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     PrismaModule,
     AuthModule,
+    TasksModule,
   ],
 })
 export class AppModule {}
